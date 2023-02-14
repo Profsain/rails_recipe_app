5536dff2e494027a449defc8836d680254d3640c
Profsain <profsaincrown@gmail.com>
2023-02-14 15:00:15 +0000

    updated devise

--- db/migrate/20230214145639_create_food.rb
+++ db/migrate/20230214145639_create_food.rb
@@ -1,4 +1,4 @@
-class CreateFoods < ActiveRecord::Migration[7.0]
+class CreateFood < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
@@ -6,7 +6,6 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.integer :price
       t.integer :quantity
       t.references :user, null: false, foreign_key: true
-      
       t.timestamps
     end
   end
