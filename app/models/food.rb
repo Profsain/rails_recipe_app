71111a4b877a275cc62a8d535b4650e099ce29b6
Profsain <profsaincrown@gmail.com>
2023-02-14 21:10:58 +0000

    fix rubocop

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,12 +1,11 @@
 class Food < ApplicationRecord
   # Associations
   belongs_to :user
-  
+
 
   # Validation
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,4 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
-  
 end
