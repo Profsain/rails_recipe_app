83c8c378a5e4445c0532bde84114b52f37772573
Profsain <profsaincrown@gmail.com>
2023-02-15 03:06:15 +0000

    created food list view

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_120709) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_145639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.float "price"
+    t.integer "price"
+    t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -78,7 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_120709) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "role", default: ""
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
