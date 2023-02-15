5df6f893dd2550851282493237241c7cbf07217d
OmarMWarraich <earthling986@gmail.com>
2023-02-15 13:37:57 +0500

    feat: Shopping List

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,19 +1,19 @@
-`class InventoryFoodsController < ApplicationController
-    def new
-        @inventory_food = InventoryFood.new
-    end
+class InventoryFoodsController < ApplicationController
+  def new
+    @inventory_food = InventoryFood.new
+  end
 
-    def create
-        @inventory = Inventory.find(params[:inventory_id])
-        @food = Food.find(params[:food_id])
-        @inventory_food = InventoryFood.new(inventory_food_params, inventory_id: @inventory.id, food_id: @food.id)
-        @inventory_food.save
-        redirect_to inventory_path(@inventory_food.inventory_id)
-    end
+  def create
+    @inventory = Inventory.find(params[:inventory_id])
+    @food = Food.find(params[:food_id])
+    @inventory_food = InventoryFood.new(**inventory_food_params, inventory_id: @inventory.id, food_id: @food.id)
+    @inventory_food.save
+    redirect_to inventory_path(@inventory_food.inventory_id)
+  end
 
-    private
+  private
 
-    def inventory_food_params
-        params.require(:inventory_food).permit(:food_id, :inventory_id, :quantity)
-    end
+  def inventory_food_params
+    params.require(:inventory_food).permit(:quantity)
+  end
 end
---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,18 @@
+class ShoppingListController < ApplicationController
+  def index
+    @user = current_user
+    @recipe = Recipe.find(params[:recipe_id])
+    @inventory = Inventory.find(params[:inventory_id])
+    @inventory_food = InventoryFood.find(params[:inventory_food_id])
+    @shopping_list = []
+    @shopping_list << @inventory_food if @inventory_food.quantity < @recipe.quantity(@recipe)
+
+    @shopping_list
+
+    @total_value = 0
+
+    @shopping_list.each do |item|
+      @total_value += item.food.price * item.quantity
+    end
+  end
+end
