955330fefbf52cc42ec959328b37207ddf60e2f1
Profsain <profsaincrown@gmail.com>
2023-02-15 03:54:36 +0000

    Added food test

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_145639) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_130430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.integer "price"
-    t.integer "quantity"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
-  end
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "inventory_foods", force: :cascade do |t|
@@ -38,8 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_145639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_id", null: false
-    t.bigint "food_id", null: false
-    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
@@ -47,9 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_145639) do
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "food_id", null: false
     t.bigint "recipe_id", null: false
-    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
@@ -61,33 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_145639) do
     t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "role", default: ""
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "foods", "users"
-  add_foreign_key "inventories", "users"
-  add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
-  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipes", "users"
 end
