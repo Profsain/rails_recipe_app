631da8748369600cee73592fe61b636c2d8e09db
OmarMWarraich <earthling986@gmail.com>
2023-02-16 20:20:31 +0500

    fix: Controller Inventory_Foods

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -4,16 +4,23 @@ class InventoryFoodsController < ApplicationController
   end
 
   def create
-    @inventory = Inventory.find(params[:inventory_id])
-    @food = Food.find(params[:food_id])
-    @inventory_food = InventoryFood.new(**inventory_food_params, inventory_id: @inventory.id, food_id: @food.id)
-    @inventory_food.save
-    redirect_to inventory_path(@inventory_food.inventory_id)
+   @inventory_food = InventoryFood.new(quantity: params[:quantity], food_id: params[:food_id])
+   @inventory_food.inventory = @inventory
+
+    if @inventory_food.save
+      redirect_to inventory_path(@inventory), notice: 'Food has been added successfully'
+    else
+      flash.now[:error] = 'Food could not be saved, try again'
+      render :new
+    end
   end
 
-  private
-
-  def inventory_food_params
-    params.require(:inventory_food).permit(:quantity)
+  def destroy
+    @inventory_food = InventoryFood.find(params[:id])
+    if @inventory_food.destroy
+      redirect_to inventory_path(@inventory), notice: 'Food was successfully destroyed.'
+    else
+      flash.now[:error] = 'Error: Food could not be deleted'
+    end
   end
 end
