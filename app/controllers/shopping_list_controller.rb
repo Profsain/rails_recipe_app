23855274f93c8f125ee4125e620114e64fa8c6f1
Profsain <profsaincrown@gmail.com>
2023-02-17 12:51:06 +0000

    Fixed Shopping List required changes

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,15 +1,17 @@
 class ShoppingListController < ApplicationController
   def index
     @user = current_user
-    @recipe = Recipe.find(params[:recipe_id])
-    @inventory = Inventory.find(params[:inventory_id])
-    @inventory_food = InventoryFood.find(params[:inventory_food_id])
+    @foods = Food.all
+    @recipes = Recipe.where(user_id: @user.id)
+    @recipe_foods = RecipeFood.where(recipe_id: @recipes.ids)
     @shopping_list = []
-    @shopping_list << @inventory_food if @inventory_food.quantity < @recipe.quantity(@recipe)
-    @total_value = 0
-
-    @shopping_list.each do |item|
-      @total_value += item.food.price * item.quantity
+    @recipe_foods.each do |recipe_food|
+      @shopping_list << recipe_food
     end
+    @final_list = @foods.where(id: @shopping_list.map(&:food_id))
+
+    @price = @final_list.pluck(:price)
+    @quantity = @final_list.pluck(:quantity)
+    @total = @price.zip(@quantity).map { |x, y| x * y }.sum
   end
 end
