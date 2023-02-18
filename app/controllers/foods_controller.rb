85295e03ad68333568dda538fd86155446a00750
Profsain <profsaincrown@gmail.com>
2023-02-18 08:10:57 +0000

    fixed food quantity error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,7 @@
 class FoodsController < ApplicationController
+  # before_action :authenticate_user!
   def index
-    @foods = Food.all.where(user_id: current_user.id)
+    @foods = Food.all
   end
 
   def show
@@ -16,6 +17,7 @@ class FoodsController < ApplicationController
       name: food_params[:name],
       measurement_unit: food_params[:measurement_unit],
       price: food_params[:price],
+      quantity: food_params[:quantity],
       user_id: current_user.id
     )
 
@@ -41,6 +43,6 @@ class FoodsController < ApplicationController
   private
 
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,11 +1,21 @@
 class ShoppingListController < ApplicationController
   def index
-    @recipe = Recipe.find(params[:recipe_id])
-    @inventory = Inventory.find(params[:inventory_id])
-    @missing_foods = []
-
-    @recipe.foods.each do |food|
-      @missing_foods.push(food) unless @inventory.foods.include?(food)
+    @user = current_user
+    @foods = Food.all
+    @recipes = Recipe.where(user_id: @user.id)
+    @recipe_foods = RecipeFood.where(recipe_id: @recipes.ids)
+    @shopping_list = []
+    @recipe_foods.each do |recipe_food|
+      @shopping_list << recipe_food
     end
+    @final_list = @foods.where(id: @shopping_list.map(&:food_id))
+
+    @price = @final_list.pluck(:price)
+    @quantity = @final_list.pluck(:quantity)
+
+    puts "@Price: #{@price.inspect}"
+    puts "@Quantity: #{@quantity.inspect}"
+    
+    @total = @price.zip(@quantity).map { |x, y| x * y }.sum
   end
 end
